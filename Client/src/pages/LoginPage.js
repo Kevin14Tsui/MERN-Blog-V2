bbf900ebe9f06a9a14d4bb867555786e0729176a
Kevin14Tsui <lungtsui15@gmial.com>
2023-02-28 23:36:28 +0800

    4235register

--- Client/src/pages/LoginPage.js
+++ Client/src/pages/LoginPage.js
@@ -1,7 +1,12 @@
 export default function LoginPage() {
   return (
     <div>
-      <h1>Login</h1>
+      <form className="login">
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
     </div>
   );
 }
