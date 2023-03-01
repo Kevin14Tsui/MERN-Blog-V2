e449f05bef40c37155cf366b54e5f700aeecd561
Kevin14Tsui <lungtsui15@gmial.com>
2023-03-01 16:16:11 +0800

    14724

---
+++ Client/src/pages/CreateaPost.js
@@ -0,0 +1,29 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export default function CreatePost() {
+  return (
+    <form
+    // onSubmit={createNewPost}
+    >
+      <input
+        type="title"
+        placeholder={"Title"}
+        // value={title}
+        // onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        // value={summary}
+        // onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input
+        type="file"
+        //   onChange={(ev) => setFiles(ev.target.files)}
+      />
+      <ReactQuill />
+      <button style={{ marginTop: "5px" }}>Create post</button>
+    </form>
+  );
+}
