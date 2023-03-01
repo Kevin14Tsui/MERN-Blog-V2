e5bfb1df70aad45844b1d7e03a43b8cc6f0ac6b5
Kevin14Tsui <lungtsui15@gmial.com>
2023-03-02 02:31:52 +0800

    lastpart

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ mongoose.connect(
 );
 
 app.post("/register", async (req, res) => {
-  const { username, password } = req.body; // from register body
+  const { username, password } = req.body;
   try {
     const userDoc = await User.create({
       username,
@@ -33,7 +33,8 @@ app.post("/register", async (req, res) => {
     });
     res.json(userDoc);
   } catch (e) {
-    res.status(400).json("wrong credentials");
+    console.log(e);
+    res.status(400).json(e);
   }
 });
 
@@ -42,20 +43,19 @@ app.post("/login", async (req, res) => {
   const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
-    // logged
+    // logged in
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
       res.cookie("token", token).json({
         id: userDoc._id,
         username,
-      }); //setcookie
+      });
     });
   } else {
-    res.status(400).json("Wrong credntials");
+    res.status(400).json("wrong credentials");
   }
 });
 
-// get info
 app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   jwt.verify(token, secret, {}, (err, info) => {
@@ -68,10 +68,9 @@ app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
 });
 
-// app post
 app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   const { originalname, path } = req.file;
-  const parts = originalname.split("."); //need the last part
+  const parts = originalname.split(".");
   const ext = parts[parts.length - 1];
   const newPath = path + "." + ext;
   fs.renameSync(path, newPath);
@@ -88,7 +87,37 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
       author: info.id,
     });
     res.json(postDoc);
-  }); //post file name
+  });
+});
+
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("you are not the author");
+    }
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
 });
 
 app.get("/post", async (req, res) => {
@@ -97,9 +126,9 @@ app.get("/post", async (req, res) => {
       .populate("author", ["username"])
       .sort({ createdAt: -1 })
       .limit(20)
-  ); //select what info u need
+  );
 });
-// post id
+
 app.get("/post/:id", async (req, res) => {
   const { id } = req.params;
   const postDoc = await Post.findById(id).populate("author", ["username"]);
@@ -107,3 +136,4 @@ app.get("/post/:id", async (req, res) => {
 });
 
 app.listen(4000);
+//
