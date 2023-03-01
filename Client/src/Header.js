937bfe85921c00fff36d86c85c85db06293547ae
Kevin14Tsui <lungtsui15@gmial.com>
2023-03-01 12:38:58 +0800

    loginV3

--- Client/src/Header.js
+++ Client/src/Header.js
@@ -1,14 +1,40 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  // save use info
+  const [username, setUsername] = useState(null);
+  // show user name
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
+  function logout() {}
+
   return (
     <header>
       <Link to="/" className="logo">
         Myblog
       </Link>
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logiut</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require("./models/User");
 const bcrypt = require("bcryptjs"); //enctype
 const app = express();
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "67tg8ffs8g6wt7gh49";
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(cookieParser());
 
 // connect mongodb
 mongoose.connect(
@@ -43,6 +45,15 @@ app.post("/login", async (req, res) => {
   } else {
     res.status(400).json("Wrong credntials");
   }
+});
+
+// get info
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
 });
 
 app.listen(4000);
